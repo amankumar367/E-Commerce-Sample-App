931e633188047253a49f7f40ed91ecca1769c5f1
AMAN KUMAR <kumaraman367@gmail.com>
2019-12-12 12:17:59 +0530

    Added Home Activity

---
+++ app/src/main/java/com/aman/ecommerce/HomeActivity.kt
@@ -0,0 +1,21 @@
+package com.aman.ecommerce
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+    companion object {
+        fun start(context: Context) {
+            val intent = Intent(context, HomeActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/aman/ecommerce/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.aman.ecommerce
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler()
+        handler.postDelayed({
+            HomeActivity.start(this)
+        }, 2000)
+
+    }
+}
